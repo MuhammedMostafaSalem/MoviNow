9f3151e44db1d824b86496184451cf07fe045e89
Muhammed Mostafa <marwansalem164@gmail.com>
2024-02-12 06:25:46 +0200

    Add withMT  in tailwind.config and screens and colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "576px",
+        "sm-max": { max: "335px" },
+        "sm-max2": { max: "500px" },
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1400px",
+      },
+      colors: {
+        orange: "#67dbda",
+      },
+    },
   },
   plugins: [],
-}
+})
 
