f8b23055463eb27b0375b791ec2e7e62e1c09a58
Muhammed Mostafa <marwansalem164@gmail.com>
2024-02-12 10:11:36 +0200

    Add MovieList in Home page

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MovieList from '../../components/MovieList'
+
+const Home = () => {
+    return (
+        <div className='pt-[100px]'>
+            <MovieList />
+        </div>
+    )
+}
+
+export default Home
