c48d6f75473188e2b6d863f59c89322b3fbfdeaa
Muhammed Mostafa <marwansalem164@gmail.com>
2024-02-17 12:04:52 +0200

    Create store and add darkMode reducer

---
+++ frontend/src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import darkModeReducer from "./reducers/darkModeReducer";
+
+export const store = configureStore({
+    reducer: {
+        darkMode: darkModeReducer,
+    },
+})
