e1846366def49e04c5de25f718fd9f789c3dcdf8
Muhammed Mostafa <marwansalem164@gmail.com>
2024-03-01 08:35:33 +0200

    Add langs, handleChangeLang and useTranslation

--- frontend/src/components/utils/Header.jsx
+++ frontend/src/components/utils/Header.jsx
@@ -3,10 +3,12 @@ import logo from '../../assets/images/play-button.png'
 import { Button, IconButton, Input } from '@material-tailwind/react'
 import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
 
-const Header = ({onToggleMode, darkMode}) => {
+const Header = ({onToggleMode, darkMode, langs, handleChangeLang}) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
+    const { t } = useTranslation();
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -47,61 +49,69 @@ const Header = ({onToggleMode, darkMode}) => {
                 <a href="#" className='flex items-center gap-[5px]'>
                     <img src={logo} alt='logo' className='w-[50px] sm-max:w-[30px]' />
                     <h2 className='font-bold text-[18px] sm-max:text-[12px] text-[orange]'>
-                    Material Tailwind
+                    {t('logo')}
                     </h2>
                 </a>
-                <div className="ml-auto flex gap-1 md:mr-4">
-                    <IconButton className={`focus:outline-none ${darkMode ? 'text-[#f9f9f9e7]' : 'text-[#222]'}`} variant="text" onClick={onToggleMode}>
+                <div className='flex items-center gap-[5px]'>
+                    <div className="ml-auto flex gap-1 md:mr-4">
+                        <IconButton className={`focus:outline-none ${darkMode ? 'text-[#f9f9f9e7]' : 'text-[#222]'}`} variant="text" onClick={onToggleMode}>
+                            {
+                                darkMode ?
+                                    <MdOutlineLightMode className="h-4 w-4" />
+                                : <MdOutlineDarkMode className="h-4 w-4" />
+                            }
+                        </IconButton>
                         {
-                            darkMode ?
-                                <MdOutlineLightMode className="h-4 w-4" />
-                            : <MdOutlineDarkMode className="h-4 w-4" />
+                            langs === "en" ?
+                                <IconButton className={`focus:outline-none ${darkMode ? 'text-[#f9f9f9e7]' : 'text-[#222]'}`} variant="text" onClick={() => handleChangeLang('ar')}>
+                                    <div className="h-4 w-4">ar</div>
+                                </IconButton>
+                            :
+                            <IconButton className={`focus:outline-none ${darkMode ? 'text-[#f9f9f9e7]' : 'text-[#222]'}`} variant="text" onClick={() => handleChangeLang('en')}>
+                                <div className="h-4 w-4">en</div>
+                            </IconButton>
                         }
-                    </IconButton>
-                    <IconButton className={`focus:outline-none ${darkMode ? 'text-[#f9f9f9e7]' : 'text-[#222]'}`} variant="text">
-                        <div className="h-4 w-4">ar</div>
-                    </IconButton>
-                </div>
-                
-                <div
-                    className={`relative
-                    flex
-                    w-full
-                    gap-2
-                    md:w-max
-                    sm-max2:fixed
-                    sm-max2:left-0
-                    sm-max2:bg-[#0c1524]
-                    sm-max2:py-[20px]
-                    sm-max2:px-[20px]
-                    sm-max2:duration-1000
-                    ${isMenuOpen ? "sm-max2:top-[65px]" : "sm-max2:-top-[80px]"}
-                    `}
-                >
-                    <Input
-                        type="search"
-                        color={darkMode ? 'white' : 'black'}
-                        label="Type here..."
-                        className="pr-20"
-                        containerProps={{
-                            className: "min-w-[288px]",
-                        }}
-                    />
-                    <Button
-                        size="sm"
-                        color={darkMode ? 'white' : 'black'}
-                        className="!absolute right-1 top-1 rounded"
+                    </div>
+                    
+                    <div
+                        className={`relative
+                        flex
+                        w-full
+                        gap-2
+                        md:w-max
+                        sm-max2:fixed
+                        sm-max2:left-0
+                        sm-max2:bg-[#0c1524]
+                        sm-max2:py-[20px]
+                        sm-max2:px-[20px]
+                        sm-max2:duration-1000
+                        ${isMenuOpen ? "sm-max2:top-[65px]" : "sm-max2:-top-[80px]"}
+                        `}
                     >
-                        Search
-                    </Button>
-                </div>
+                        <Input
+                            type="search"
+                            color={darkMode ? 'white' : 'black'}
+                            label={t('TypeHereSearch')}
+                            containerProps={{
+                                className: "min-w-[288px]",
+                            }}
+                        />
+                        <Button
+                            size="sm"
+                            color={darkMode ? 'white' : 'black'}
+                            className={`!absolute top-1 rounded ${langs === "en" ? 'right-1' : 'left-1'}`}
+                        >
+                            {t('Search')}
+                        </Button>
+                    </div>
 
-                <div className='hidden sm-max2:block text-xl' onClick={toggleMenu}>
-                    {
-                        !isMenuOpen ?
-                            <FaBars />
-                        : <FaTimes />
-                    }
+                    <div className='hidden sm-max2:block text-xl' onClick={toggleMenu}>
+                        {
+                            !isMenuOpen ?
+                                <FaBars />
+                            : <FaTimes />
+                        }
+                    </div>
                 </div>
             </div>
         </header>
