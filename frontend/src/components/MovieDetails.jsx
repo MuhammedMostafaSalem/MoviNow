325b1ce08df1f2bbb3aa1eb0b77d42c9e600205b
Muhammed Mostafa <marwansalem164@gmail.com>
2024-03-01 08:32:46 +0200

    Add useSelector(language)

--- frontend/src/components/MovieDetails.jsx
+++ frontend/src/components/MovieDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CircleRating from './circleRating/CircleRating';
 import PlayIcon from './PlayIcon';
 import {
@@ -6,21 +6,74 @@ import {
     Dialog,
     DialogHeader,
     DialogBody,
-    DialogFooter,
     Typography
 } from "@material-tailwind/react";
 import { FaTimes } from 'react-icons/fa';
+import { ApiKey } from '../config/configToken';
+import { useParams } from 'react-router-dom';
+import baseURL from '../api/baseURL';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
 const MovieDetails = ({darkMode}) => {
+    const { t } = useTranslation();
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(!open);
+
+    const Param= useParams();
+    const [movie, setMovie] = useState([])
+    const { language } = item(state => state.lang);
+
+    // get movie details with axios
+    const getMovieDetails= async()=> {
+        const res = await baseURL.get(`/movie/${Param.id}?api_key=${ApiKey}&language=${language}`)
+        setMovie(res.data);
+    }
+
+    useEffect(() => {
+        if(movie) {
+            getMovieDetails();
+        }
+    }, [movie])
+
+    let subtitle = []
+    try {
+        if(movie.genres) {
+            subtitle = movie.genres;
+        } else {
+            subtitle
+        }
+    } catch(err) {}
+
+    // get watch movie
+    const [watchMovie, setWatchMovie] = useState({});
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await baseURL.get(`/movie/${Param.id}/watch/providers?api_key=${ApiKey}`);
+            setWatchMovie(response.data.results);
+        };
+    
+        fetchData();
+    }, []);
+
+    let viedoLink = []
+    try {
+        if(watchMovie.AR.link) {
+            viedoLink = watchMovie.AR.link;
+        } else {
+            viedoLink
+        }
+    } catch(err) {}
+
+    // open watch movie in new tap
+    const videoNewTap = url => window.open(url)
 
     return (
         <div className='w-full pt-[100px]'>
             <div className='w-full h-full absolute top-0 left-0 opacity-[0.1] overflow-hidden'>
                 <img
                     className="posterImg w-full h-full object-cover object-center"
-                    src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                    src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
                     alt=""
                 />
             </div>
@@ -33,7 +86,7 @@ const MovieDetails = ({darkMode}) => {
                     <div className="shrink-0">
                         <img
                             className='w-full block rounded-[12px] md:max-w-[350px]'
-                            src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                             alt=""
                         />
                     </div>
@@ -42,85 +95,70 @@ const MovieDetails = ({darkMode}) => {
                         ${darkMode ? 'text-[#f9f9f9]' : 'text-[#222]'}
                     `}>
                         <div className='title text-[28px] leading-[40px] md:text-[34px] md:leading-[44px]'>
-                            name or title
+                            {movie.title}
                         </div>
-                        <div className="subtitle text-[16px] leading-[24px] mb-[15px] italic opacity-[0.5] md:text-[20px] md:leading-[28px]">
-                            subtitle
+                        <div className='flex gap-5'>
+                            {
+                                subtitle.map((item, index) => (
+                                    <div className="subtitle text-[16px] leading-[24px] mb-[15px] italic opacity-[0.5] md:text-[20px] md:leading-[28px]" key={index}>
+                                        {item.name}
+                                    </div>
+                                ))
+                            }
                         </div>
 
                         <div className="flex items-center gap-[25px] mb-[25px]">
                             <CircleRating
-                                rating='3.5'
+                                rating={movie.vote_average}
                             />
                             <div
                                 className={`
                                     ${darkMode ? 'playLightMode' : 'playDarkMode'}
                                     playbtn playLightMode flex items-center gap-[20px] cursor-pointer
                                 `}
+                                onClick={() => videoNewTap(viedoLink)}
                             >
-                                <PlayIcon onOpenDialog={handleOpen} />
+                                <PlayIcon />
                                 <span className="text">
-                                    Watch Now
+                                {t('WatchNow')}
                                 </span>
                             </div>
                         </div>
 
                         <div className="overview mb-[25px] ">
                             <div className="heading text-[24px] mb-[10]">
-                                Overview
+                                {t('Overview')}
                             </div>
                             <div className="description leading-[24px] md:pr-[100px]">
-                                overview
+                                {movie.overview}
                             </div>
                         </div>
 
                         <div className="info">
                             <div className="infoItem">
                                 <span className="text bold">
-                                    Status:{" "}
+                                    {t('Status')}:
                                 </span>
                                 <span className="text">
-                                    status
+                                    {movie.status}
                                 </span>
                             </div>
                             <div className="infoItem">
                                 <span className="text bold">
-                                    Release Date:{" "}
+                                    {t('ReleaseDate')}:
                                 </span>
                                 <span className="text">
-                                    release_date
+                                    {movie.release_date}
                                 </span>
                             </div>
                             <div className="infoItem">
                                 <span className="text bold">
-                                    Runtime:{" "}
+                                    {t('Runtime')}:
                                 </span>
                                 <span className="text">
-                                    runtime
+                                    {movie.runtime}
                                 </span>
                             </div>
-                        </div>
-
-                        <div className="info">
-                            <span className="text bold">
-                                Director:{" "}
-                            </span>
-                            <span className="text">
-                                <span>
-                                    director
-                                </span>
-                            </span>
-                        </div>
-
-                        <div className="info">
-                            <span className="text bold">
-                                Writer:{" "}
-                            </span>
-                            <span className="text">
-                                <span>
-                                    writer
-                                </span>
-                            </span>
                         </div>
                     </div>
                 </div>
