4f896413d021546556c3da37c7adbe1f67f42659
Muhammed Mostafa <marwansalem164@gmail.com>
2024-03-01 08:49:43 +0200

    Add movies and lang

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import darkModeReducer from "./reducers/darkModeReducer";
+import moviesReducer from "./reducers/moviesReducer";
+import languageReducer from "./reducers/languageReducer";
 
 export const store = configureStore({
     reducer: {
         darkMode: darkModeReducer,
+        movies: moviesReducer,
+        lang: languageReducer,
     },
+    devTools: true,
 })
