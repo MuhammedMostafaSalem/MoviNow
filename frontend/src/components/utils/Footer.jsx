d8e022e33ab114b3ba4f2f003965490372258f76
Muhammed Mostafa <marwansalem164@gmail.com>
2024-03-01 08:34:36 +0200

    Remove console.log

--- frontend/src/components/utils/Footer.jsx
+++ frontend/src/components/utils/Footer.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const Footer = ({darkMode}) => {
     const [year, setYear] = useState(new Date().getFullYear());
-console.log(darkMode)
+
     return (
         <div className={`
             ${darkMode ? 'bg-[#222] text-[#f9f9f9e7]' : 'bg-[#f9f9f9e7] text-[#222]'}
