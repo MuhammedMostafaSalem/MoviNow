c3083c2796f12e41d537e9629801ad94a28840a8
Muhammed Mostafa <marwansalem164@gmail.com>
2024-03-01 08:30:24 +0200

    Wrap I18nextProvider with App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,17 @@ import './index.css'
 import { ThemeProvider } from '@material-tailwind/react'
 import { Provider } from 'react-redux';
 import { store } from './Store/store.js'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <Provider store={store}>
-        <App />
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>,
