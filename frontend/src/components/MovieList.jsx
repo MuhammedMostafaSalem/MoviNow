3aa5a8c3b419ec3558ea3fbe3d63ae03fb024d38
Muhammed Mostafa <marwansalem164@gmail.com>
2024-03-01 08:33:35 +0200

    Add darkMode , language and movies

--- frontend/src/components/MovieList.jsx
+++ frontend/src/components/MovieList.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CardMovie from './CardMovie'
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { getAllMovies } from '../store/actions/moviesAction';
 
-const MovieList = () => {
+const MovieList = ({darkMode}) => {
+    const dispatch = useDispatch()
+    const { movies } = useSelector((state) => state.movies);
+    const { language } = useSelector(state => state.lang);
+    useEffect(() => {
+        if(movies) {
+            dispatch(getAllMovies(language));
+        }
+    }, [movies, dispatch]);
+
+    let moviesData = []
+    try {
+        if(movies.results) {
+            moviesData = movies.results;
+        } else {
+            moviesData
+        }
+    } catch(err) {}
+
     return (
         <div className='container mx-auto'>
             <div
@@ -15,22 +36,17 @@ const MovieList = () => {
                 overflow-y-auto 
                 sm-max:px-2'
             >
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
-                <CardMovie />
+                {
+                    moviesData ?
+                        (
+                            moviesData.map((item, index) => (
+                                <CardMovie key={index} item={item} />
+                            ))
+                        )
+                    : (
+                        <h2 className={`capitalize text-center ${darkMode ? 'text-[#f9f9f9e7]' : 'text-[#222]'} mt-5 p-5`}>there are no movies</h2>
+                    )
+                }
             </div>
         </div>
     )
