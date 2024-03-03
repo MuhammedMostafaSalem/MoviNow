7adf87bce5bc3f8ad947ee1f87da8a093b3993e2
Muhammed Mostafa <marwansalem164@gmail.com>
2024-03-03 20:19:53 +0200

    Create getMoviesPage

--- frontend/src/store/actions/moviesAction.js
+++ frontend/src/store/actions/moviesAction.js
@@ -11,3 +11,13 @@ export const getAllMovies = createAsyncThunk('movies/getMovies', async (language
         throw error; // Re-throw for error handling in the reducer
     }
 });
+
+export const getMoviesPage = createAsyncThunk('movies/getMoviesPage', async ({ language, page }) => {
+    try {
+        const response = await useGetDataToken(`/movie/popular?api_key=${ApiKey}&language=${language}&page=${page}`);
+        return response.data;
+    } catch (error) {
+        console.log(error)
+        throw error; // Re-throw for error handling in the reducer
+    }
+});
